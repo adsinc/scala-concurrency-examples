4096047af621aaf4d9b57aae22cb64cf2a217dac
adsinc <sinc86@gmail.com>
2018-03-18 19:36:30 +0300

    chapter3 pure lazy cell

--- src/main/scala/learningconcurrency/tasks/ch3/LazyCell.scala
+++ src/main/scala/learningconcurrency/tasks/ch3/LazyCell.scala
@@ -1,7 +1,19 @@
-package learningconcurrency.tasks.ch3
+package learningconcurrency.tasks
+package ch3
+
+import java.util.concurrent.atomic.AtomicReference
+
+import scala.annotation.tailrec
 
 class LazyCell[T](init: => T) {
-  def apply(): T = init
+  private var value: Option[T] = None
+
+  def apply(): T = value.synchronized {
+    if (value.isEmpty) {
+      value = Some(init)
+    }
+    value.get
+  }
 }
 
 object LazyCellTest extends App {
@@ -10,5 +22,33 @@ object LazyCellTest extends App {
     1
   })
   println("Lazy cell created")
-  println(s"Get lazy cell value: ${lc()}")
+
+  (for (i <- 1 to 8) yield thread {
+    println(s"Get lazy cell value: ${lc()}")
+  }).foreach(_.join)
+}
+
+class PureLazyCell[T](init: => T) {
+  private val r = new AtomicReference[Option[T]](None)
+
+  @tailrec
+  final def apply(): T = {
+    val value = r.get()
+    value match {
+      case Some(v) => v
+      case None =>
+        val value = init
+        if (!r.compareAndSet(None, Some(value))) apply()
+        else value
+    }
+  }
+}
+
+object PureLazyCellTest extends App {
+  val lc = new PureLazyCell[Long](System.nanoTime())
+  println("Lazy cell created")
+
+  (for (i <- 1 to 10) yield thread {
+    println(s"Get lazy cell value: ${lc()}")
+  }).foreach(_.join)
 }
