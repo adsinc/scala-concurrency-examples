58b6db3eee5e79313b9eebaee15ee47231c1af96
adsinc <sinc86@gmail.com>
2018-04-14 13:20:50 +0300

    chapter 6

--- src/main/scala/learningconcurrency/tasks/ch6/RandomQuote.scala
+++ src/main/scala/learningconcurrency/tasks/ch6/RandomQuote.scala
@@ -8,11 +8,22 @@ import scala.io.Source
 object RandomQuote extends App {
 
   def randomQuote = Observable[String] { obs =>
+    def extractQuote(q: String): String = {
+      """<p>(.*)<""".r.findAllIn(q).group(1)
+    }
     val url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
-    obs.onNext(Source.fromURL(url).getLines().mkString)
+    obs.onNext(extractQuote(Source.fromURL(url).getLines().mkString))
     obs.onCompleted()
     Subscription()
   }
 
-  randomQuote.subscribe(log _)
+  randomQuote
+    .map(_.length)
+    .repeat(5)
+    .scan((0, 0)) {
+      case ((oldLen, cnt), l) => (oldLen + l, cnt + 1)
+    }
+    .tail
+    .map { case (sum, cnt) => sum.toDouble / cnt }
+    .subscribe(s => log(s.toString))
 }
