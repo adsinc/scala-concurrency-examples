1aabbe16ffcd1d5766a8ef036b924dc6c971b3ac
Aleksei Dolgii <sinc86@gmail.com>
2018-05-01 15:37:17 +0300

    stm

---
+++ src/main/scala/learningconcurrency/tasks/ch7/TPairTest.scala
@@ -0,0 +1,58 @@
+package learningconcurrency.tasks.ch7
+
+import learningconcurrency.tasks._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.stm._
+
+object TPairTest extends App {
+
+  class TPair[P, Q](pInit: P, qInit: Q) {
+    private val p = Ref(pInit)
+    private val q = Ref(qInit)
+
+    def first(implicit txn: InTxn): P = p.single()
+
+    def first_=(x: P)(implicit txn: InTxn): P = p.single swap x
+
+    def second(implicit txn: InTxn): Q = q.single()
+
+    def second_=(y: Q)(implicit txn: InTxn): Q = q.single swap y
+
+    def swap()(implicit e: P =:= Q, e2: Q =:= P,txn: InTxn): Unit = atomic { tx =>
+      val tmp = q()
+      q() = p()
+      p() = tmp
+    }
+  }
+
+  val p = new TPair(0, "empty")
+
+  def printPair[P, Q](p: TPair[P, Q]): Unit = atomic { implicit tx =>
+    log(s"First=${p.first}")
+    log(s"Second=${p.second}")
+  }
+
+  Future {
+    atomic { implicit tx =>
+      p.first = 1
+      p.second = "Hello"
+    }
+  }
+
+  Future {
+    atomic { implicit tx =>
+      p.first = 2
+      p.second = "World"
+    }
+  }
+  Thread.sleep(100)
+  printPair(p)
+
+  val p1 = new TPair(1, 2)
+  atomic { implicit tx =>
+    p1.swap()
+  }
+  printPair(p1)
+}
