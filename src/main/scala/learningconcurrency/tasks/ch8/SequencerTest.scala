8696a802625c8712013c5c033b6be17dd60e822a
Aleksei Dolgii <sinc86@gmail.com>
2018-06-09 23:42:47 +0300

    akka

--- src/main/scala/learningconcurrency/tasks/ch8/SequencerTest.scala
+++ src/main/scala/learningconcurrency/tasks/ch8/SequencerTest.scala
@@ -1,36 +1,54 @@
 package learningconcurrency.tasks.ch8
 
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import learningconcurrency.tasks.ch8.Sequencer.SequencedMsg
 
+import scala.annotation.tailrec
 import scala.collection.immutable.TreeMap
+import scala.util.Random
 
-class Sequencer(dest: ActorRef) extends Actor {
+class Sequencer(dest: ActorRef) extends Actor with ActorLogging {
   def receive: Receive = onMessage(0)
 
-  private def onMessage(nextNumber: Long, derived: TreeMap[Long, Any] = TreeMap.empty): Receive = {
-    case msg@(num: Long, _) =>
-      if (num == nextNumber) {
+  private def onMessage(nextNumber: Long, derived: TreeMap[Long, SequencedMsg] = TreeMap.empty): Receive = {
+    case msg@SequencedMsg(num, _) =>
+      if(num < nextNumber) {
+        log.error(s"Repeated message number. Expected: $nextNumber, actual: $num")
+      } else if (num == nextNumber) {
         dest ! msg
-        var newNextNumber = nextNumber + 1
-        context become onMessage(newNextNumber)
+        val (newNextNumber, newDerived) = trySendDerived(nextNumber + 1, derived)
+        context become onMessage(newNextNumber, newDerived)
       } else {
-        context become onMessage(nextNumber, derived + msg)
+        context become onMessage(nextNumber, derived + (msg.seqNum -> msg))
       }
     case msg => dest ! msg
   }
-}
 
-class ConsumerActor extends Actor with ActorLogging {
-  def receive: Receive = {
-    case msg => log.info(s"Received: $msg")
+  @tailrec
+  private def trySendDerived(nextNumber: Long, derived: TreeMap[Long, SequencedMsg]): (Long, TreeMap[Long, SequencedMsg]) = {
+    if(derived.isEmpty || derived.firstKey > nextNumber)
+      (nextNumber, derived)
+    else {
+      dest ! derived.head._2
+      trySendDerived(nextNumber + 1, derived.tail)
+    }
   }
 }
 
-object FlowRateActorTest extends App {
+object Sequencer {
+  case class SequencedMsg(seqNum: Long, msg: Any)
+}
+
+object SequencerActorTest extends App {
   val actorSystem = ActorSystem()
   val consumer = actorSystem.actorOf(Props[ConsumerActor])
   val sequencer = actorSystem.actorOf(Props(new Sequencer(consumer)))
 
+  for {
+    i <- Random.shuffle(0 to 50)
+  } sequencer ! SequencedMsg(i, s"Message $i")
+
+  Thread.sleep(1000)
 
   actorSystem.terminate()
 }
