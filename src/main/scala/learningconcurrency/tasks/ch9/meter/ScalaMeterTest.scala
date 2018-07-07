1acedcf9c8159f3a0e11ff547a66bcfa67ec2b93
adsinc <sinc86@gmail.com>
2018-07-08 01:26:27 +0300

    scalameter

--- src/main/scala/learningconcurrency/tasks/ch9/meter/ScalaMeterTest.scala
+++ src/main/scala/learningconcurrency/tasks/ch9/meter/ScalaMeterTest.scala
@@ -1,6 +1,7 @@
-package learningconcurrency.tasks.ch9.meter
+package learningconcurrency.tasks
+package ch9.meter
 
-import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.atomic.{AtomicLong, AtomicLongArray, AtomicReference}
 
 import org.scalameter._
 
@@ -22,8 +23,8 @@ object ScalaMeterTest extends App {
 
   val accTime = config(
     Key.exec.minWarmupRuns -> 20,
-    Key.exec.maxWarmupRuns -> 100,
-    Key.exec.benchRuns -> 1000,
+    Key.exec.maxWarmupRuns -> 50,
+    Key.exec.benchRuns -> 30,
     Key.verbose -> true
   ) withWarmer new Warmer.Default measure {
     val acc = new Accumulator(0)(_ + _)
@@ -34,7 +35,82 @@ object ScalaMeterTest extends App {
       i += 1
     }
   }
-
   println(s"Accumulator time: " + accTime)
+  println("*" * 80)
+
+  class AccumulatorLong(z: Long)(op: (Long, Long) => Long) {
+    private val value = new AtomicLong(z)
+
+    def apply(): Long = value.get()
+
+    @tailrec final def add(v: Long): Unit = {
+      val ov = value.get()
+      val nv = op(ov, v)
+      if (!value.compareAndSet(ov, nv)) add(v)
+    }
+  }
+
+  val accTimeLong = config(
+    Key.exec.minWarmupRuns -> 20,
+    Key.exec.maxWarmupRuns -> 50,
+    Key.exec.benchRuns -> 30,
+    Key.verbose -> true
+  ) withWarmer new Warmer.Default measure {
+    val acc = new AccumulatorLong(0)(_ + _)
+    var i = 0
+    val total = 1000000
+    while (i < total) {
+      acc.add(i)
+      i += 1
+    }
+  }
+
+  println(s"Accumulator time: $accTimeLong")
+  println("*" * 80)
+
+  val longAccTime4 = config(
+    Key.exec.minWarmupRuns -> 20,
+    Key.exec.maxWarmupRuns -> 50,
+    Key.exec.benchRuns -> 30,
+    Key.verbose -> true
+  ) withWarmer new Warmer.Default measure {
+    val acc = new AccumulatorLong(0)(_ + _)
+    val total = 1000000
+    val p = 4
+    val threads = for {
+      j <- 0 until p
+    } yield thread {
+      val start = j * total / p
+      var i = start
+      while (i < start + total / p) {
+        acc.add(i)
+        i += 1
+      }
+    }
+    threads foreach (_.join)
+  }
+
+  println(s"4 threads long accumulator time: $longAccTime4")
+  println("*" * 80)
+
+  import scala.util.hashing
+
+  class ParAccumulatorLong(z: Long)(op: (Long, Long) => Long) {
+    private val par = Runtime.getRuntime.availableProcessors() * 128
+    private val values = new AtomicLongArray(par)
+
+    def apply(): Long = {
+      val total = z
+      for ()
+    }
+
+    @tailrec final def add(v: Long): Unit = {
+      val id = Thread.currentThread().getId.toInt
+      val pos = math.abs(hashing.byteswap32(id)) % par
+      val ov = values.get(pos)
+      val nv = op(ov, v)
+      if (!values.compareAndSet(pos, ov, nv)) add(v)
+    }
+  }
 
 }
